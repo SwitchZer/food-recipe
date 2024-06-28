2f89ca17778e32e9855bf651307a587821e0b9c4
SwitchZer <alfredzagoto21@gmail.com>
2024-06-28 16:13:18 +0700

    Fix

--- src/middleware.js
+++ src/middleware.js
@@ -4,14 +4,14 @@ import { getCookie } from "@/service/utils";
 export function middleware(request) {
   const token = getCookie("token");
   // private route
-  // if (
-  //   request.nextUrl.pathname.startsWith("/profile") ||
-  //   request.nextUrl.pathname.startsWith("/home")
-  // ) {
-  //   if (!token) {
-  //     return NextResponse.redirect(new URL("/login", request.url));
-  //   }
-  // }
+  if (
+    request.nextUrl.pathname.startsWith("/profile") ||
+    request.nextUrl.pathname.startsWith("/home")
+  ) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+  }
 }
 
 export const config = {
